0229cd94bff12781acfd538fb838d79ef1c85170
Charles <137882382+ClsApps@users.noreply.github.com>
2023-07-26 10:36:29 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
-	alert("We are in Mail now");
-	echo "We are in Mail now";
+	//alert("We are in Mail now");
+	//echo "We are in Mail now";
 
 	 
 	$name = $_POST['name'];
@@ -12,11 +12,12 @@
 	$subject = "Mail From Your website";
 	$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone =" . $phone . "\r\n Message =" . $message;
 	$headers = "From: cls.x.me@gmail.com" . "\r\n" . "CC: cls.apps.me@gmail.com";
+mail($to,$subject,$txt,$headers);
 
-	if(mail($to,$subject,$txt,$headers)) echo "Mail was successfully sent" . "\r\n";
-	else echo "Mail Failled to send your Message" . "\r\n";
+	//if(mail($to,$subject,$txt,$headers)) echo "Mail was successfully sent" . "\r\n";
+	//else echo "Mail Failled to send your Message" . "\r\n";
 	
-	echo "Mail PHP End Just a Test"  . "\r\n";
+	//echo "Mail PHP End Just a Test"  . "\r\n";
 	//alert("Mail PHP End Just a Test");
 	//header("Location:success.html");
 ?>
