76560a97823ae18da20a9f8261b68e61c6f35b96
Charles <137882382+ClsApps@users.noreply.github.com>
2023-07-26 09:44:22 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,22 @@
+<?php
+	//alert("We are in Mail.php now");
+	echo "We are in Mail.php now";
+
+	 
+	$name = $_POST['name'];
+	$email= $_POST['email'];
+	$phone= $_POST['phone'];
+	$message= $_POST['message'];
+
+	$to = "cls.apps.mail@gmail.com";
+	$subject = "Mail From Your website";
+	$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone =" . $phone . "\r\n Message =" . $message;
+	$headers = "From: cls.x.me@gmail.com" . "\r\n" . "CC: cls.apps.me@gmail.com";
+
+	if(mail($to,$subject,$txt,$headers)) echo "Mail was successfully sent" . "\r\n";
+	else echo "Mail Failled to send your Message" . "\r\n";
+	
+	echo "Mail PHP End Just a Test"  . "\r\n";
+	//alert("Mail PHP End Just a Test");
+	//header("Location:success.html");
+?>
